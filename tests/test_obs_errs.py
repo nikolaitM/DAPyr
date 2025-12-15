131296828fd4259d16474360f8daa785044ecca6
Henry Santer <etto@dhcp-10-174-228-254.wireless.dyn.umd.edu>
2025-12-15 15:30:03 -0500

    fix for obs error testing

--- tests/test_obs_errs.py
+++ tests/test_obs_errs.py
@@ -45,35 +45,71 @@ class TestObsErrors(unittest.TestCase):
       def test_assim_change_true_obs_err_dist(self):
 
             xa_true = np.loadtxt('./tests/states/xa_true_obs_error_sd_gaussian.txt', delimiter = ',')
-            true_obs_params = {'mu1': -2.0, 'sigma1': 1, 'mu2': 3.0, 'sigma2':.5, 'threshold': 0}
+            
             true_obs_dist = 1
+            true_obs_params = {'mu1': -2.0, 'sigma1': 1, 'mu2': 3.0, 'sigma2':.5, 'threshold': 0}
+            assumed_obs_params = {'mu' : 0, 'sigma': 1}
+            assumed_obs_dist = 0
 
-            e_true_sdg = dap.Expt('test1', {'expt_flag': 1,
-                                   'model_flag':1,
-                                   'seed': 1,
-                                   'true_obs_err_dist': true_obs_dist,
-                                   'true_obs_err_params': true_obs_params,
-                                   'Ne':20,
-                                   'T': 5})
-            dap.runDA(e_true_sdg)
-            xa = e_true_sdg.x_ens.flatten()
+            rng = np.random.default_rng(58)
+            xt = rng.normal(size=(40,20))
+            xf = rng.normal(size=(40,20))
+
+            Y = xt + dap.OBS_ERRORS.sample_errors(xt, true_obs_dist, true_obs_params, rng)
+            Y = Y[:,0]
+
+            L = dap.OBS_ERRORS.get_likelihood(assumed_obs_dist, assumed_obs_params)
+            Nx, Ne = xf.shape
+            Ny = Nx
+            H = np.eye(Nx)
+            C = np.ones((Nx, Nx))
+            e_flag = 0
+            qcpass = np.zeros((Ny,))
+            xm = np.mean(xf, axis = -1)[:, None]
+
+            N_eff = 0.4
+            kddm_flag = 0
+            maxiter = 1
+            alpha = 0.3
+            min_res = 0.0
+            xa, e_flag = dap.DA.lpf_update(xf, xf, Y[:, None], H, C, N_eff*Ne, alpha, 
+                                           min_res, maxiter, kddm_flag, e_flag, qcpass, L)
+
             self.assertTrue(np.allclose(xa_true, xa))
-
+            
       def test_assim_change_assumed_obs_err_dist(self):
 
             xa_true = np.loadtxt('./tests/states/xa_assumed_obs_error_sd_gaussian.txt', delimiter = ',')
-            assumed_obs_params = {'mu1': -2.0, 'sigma1': 1, 'mu2': 3.0, 'sigma2':.5, 'threshold': 0}
+            
+            true_obs_params = {'mu' : 0, 'sigma': 1}
+            true_obs_dist = 0
             assumed_obs_dist = 1
+            assumed_obs_params = {'mu1': -2.0, 'sigma1': 1, 'mu2': 3.0, 'sigma2':.5, 'threshold': 0}
 
-            e_assumed_sdg = dap.Expt('test1', {'expt_flag': 1,
-                                   'model_flag':1,
-                                   'seed': 1,
-                                   'assumed_obs_err_dist': assumed_obs_dist,
-                                   'assumed_obs_err_params': assumed_obs_params,
-                                   'Ne':20,
-                                   'T': 5})
-            dap.runDA(e_assumed_sdg)
-            xa = e_assumed_sdg.x_ens.flatten()
+            rng = np.random.default_rng(58)
+            xt = rng.normal(size=(40,20))
+            xf = rng.normal(size=(40,20))
+
+            Y = xt + dap.OBS_ERRORS.sample_errors(xt, true_obs_dist, true_obs_params, rng)
+            Y = Y[:,0]
+
+            L = dap.OBS_ERRORS.get_likelihood(assumed_obs_dist, assumed_obs_params)
+            Nx, Ne = xf.shape
+            Ny = Nx
+            H = np.eye(Nx)
+            C = np.ones((Nx, Nx))
+            e_flag = 0
+            qcpass = np.zeros((Ny,))
+            xm = np.mean(xf, axis = -1)[:, None]
+
+            N_eff = 0.4
+            kddm_flag = 0
+            maxiter = 1
+            alpha = 0.3
+            min_res = 0.0
+            xa, e_flag = dap.DA.lpf_update(xf, xf, Y[:, None], H, C, N_eff*Ne, alpha, 
+                                           min_res, maxiter, kddm_flag, e_flag, qcpass, L)
+
             self.assertTrue(np.allclose(xa_true, xa))
 
 if __name__ == '__main__':
